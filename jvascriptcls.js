80221c5fd827616de3065f46f950874819d1d0f4
Shemidavid <123367013+Shemidavid@users.noreply.github.com>
2023-02-23 03:58:27 -0900

    Add files via upload

---
+++ jvascriptcls.js
@@ -0,0 +1,21 @@
+
+let firstName = 'dele'
+let lastName = 'giwa'
+let country = 'nigeria'
+let city = 'lagos'
+let language = 'I love javascript'
+
+let fullname = firstname + lastname + country + city + language
+console.log(fullname) 
+
+
+
+
+let player = 'Lionel Messi';
+let trophy = 'is a World Cup'
+let acc =  'winner'
+let spa = ' '
+let sent = "Lionel Messi is a world cup winner"
+
+let sentence = player +' '+ trophy + spa + acc
+console.log(sentence)
